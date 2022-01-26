d0e417bcbd165938c6cc2cee378bd01d28ecb6e2
Stanley Pan <imoment.developer@gmail.com>
2022-01-26 17:12:25 -0500

    Initial Commit

---
+++ Marquee_Animation/Marquee_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marquee_AnimationApp.swift
+//  Marquee_Animation
+//
+//  Created by Stanley Pan on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Marquee_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
