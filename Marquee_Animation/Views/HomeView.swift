87116ab956958e132c24bee54db68092bb482f07
Stanley Pan <imoment.developer@gmail.com>
2022-01-26 18:11:15 -0500

    Added fading UI to Marquee text, continuous looping with Timer, padding

--- Marquee_Animation/Views/HomeView.swift
+++ Marquee_Animation/Views/HomeView.swift
@@ -24,10 +24,11 @@ struct HomeView: View {
                         .cornerRadius(15)
                 }
                 .frame(height: 220)
+                .padding(.horizontal)
                 
                 Marquee(text: "Tech, video games, failed cooking attempts, vlogs and more!", font: .systemFont(ofSize: 16, weight: .regular))
             }
-            .padding()
+            .padding(.vertical)
             .navigationTitle("Marquee Prototype")
         }
     }
@@ -41,11 +42,17 @@ struct HomeView_Previews: PreviewProvider {
 
 // MARK: Marquee Text View
 struct Marquee: View {
-    var text: String
+    @State var text: String
     var font: UIFont
     
     @State var storedTextSize: CGSize = .zero
     @State var offset: CGFloat = 0
+    
+    // MARK: Animation Speed
+    var animationSpeed: Double = 0.01
+    var delayTime: Double = 0.5 // Set 0 for continuous
+    
+    @Environment(\.colorScheme) var scheme
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
@@ -53,11 +60,65 @@ struct Marquee: View {
             Text(text)
                 .font(Font(font))
                 .offset(x: offset)
+                .padding(.horizontal, 15)
         }
+        // Opacity Effect
+        .overlay(content: {
+            HStack {
+                let color: Color = scheme == .dark ? Color.black : Color.white
+                
+                LinearGradient(colors:
+                                [color,
+                                 color.opacity(0.7),
+                                 color.opacity(0.5),
+                                 color.opacity(0.3)],
+                               startPoint: .leading,
+                               endPoint: .trailing).frame(width: 20)
+                
+                Spacer()
+                
+                LinearGradient(colors:
+                                [color,
+                                 color.opacity(0.7),
+                                 color.opacity(0.5),
+                                 color.opacity(0.3)].reversed(),
+                               startPoint: .leading,
+                               endPoint: .trailing).frame(width: 20)
+            }
+        })
         // Disabling Manual Scrolling
         .disabled(true)
         .onAppear {
+            // Base Text
+            let baseText = text
+            
+            // MARK: Continuous Animation
+            (1...15).forEach { _ in
+                text.append(" ")
+            }
+            // Stopping Animation, before next text
             storedTextSize = textSize()
+            text.append(baseText)
+            
+            
+            // Calculating total seconds based on text width
+            let timing: Double = (animationSpeed * storedTextSize.width)
+            
+            // Delaying First Animation
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                withAnimation(.linear(duration: timing)) {
+                    offset = -storedTextSize.width
+                }
+            }
+        }
+        // MARK: Timer for continuous repeat
+        // Optional: If delay is needed for next animation
+        .onReceive(Timer.publish(every: ((animationSpeed * storedTextSize.width) + delayTime), tolerance: nil, on: .main, in: .default).autoconnect()) { _ in
+            // Reset offset to 0, to look like looping
+            offset = 0
+            withAnimation(.linear(duration: (animationSpeed * storedTextSize.width))) {
+                offset = -storedTextSize.width
+            }
         }
     }
     // MARK: Fetch Text Size for Offset Animation
